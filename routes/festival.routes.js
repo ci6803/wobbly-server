e25f697cf39dc6c9a30268c8c7b01fa9e067dc95
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-08 13:24:24 +0200

    list festivals added

--- routes/festival.routes.js
+++ routes/festival.routes.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
+const Festival = require("../models/Festival.model");
+
+// GET -festival-
+router.get('/festival', (req, res) => {
+    Festival.find()
+            .then(festivals => res.json(festivals))
+            .catch(err => res.json(err));
+});
+
+// POST -festival-
+router.post('/festival', (req, res) => {
+    const { name, image, description, type, startDate, endDate } = req.body;
+
+    Festival.create({ name, image, description, type, startDate, endDate, comments: []})
+            .then(festival => res.json(festival))
+            .catcH(err => res.json(err))
+});
 
 module.exports = router;
