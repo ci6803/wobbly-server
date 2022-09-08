1ac1dbff3c0ba1d5746b267c2ee7a4fe725c3e72
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-08 12:23:48 +0100

    no empty input

---
+++ routes/auth.routes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bcrypt = require('bcrypt');
+
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User.model");
+
+const router = express.Router();
+
+const { isAuthenticated } = require('../middleware/jwt.middleware');
+
+const saltRounds = 10;
+
+router.post('/signup', (req,res) => {
+    const { email, password, username, name } = req.body;
+
+    if (email === '' || password === '' || name === ''|| username === '') {
+        res.status(400).json({ message: "Please provide email, name, username and password"});
+        return;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
