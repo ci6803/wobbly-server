c0ca30ab9337e3113f83a806261912757e148be9
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-09 11:34:46 +0100

    display error message

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -25,7 +25,7 @@ router.post('/signup', (req,res) => {
         
     const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
     if (!passwordRegex.test(password)) {
-        res.status(400).json({message: 'assword must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.'});
+        res.status(400).json({message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.'});
         return;
     }
 
