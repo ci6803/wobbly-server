857cfbc7388d6e9e6b21e96474f36b3a9ad3fdf9
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-14 17:20:00 +0100

    cors policy

--- config/index.js
+++ config/index.js
@@ -20,12 +20,16 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
+  // app.use(
+  //   cors({
+  //     credentials: true,
+  //     origin: process.env.ORIGIN || "http://localhost:3000",
+  //   })
+  // );
+
   app.use(
-    cors({
-      credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
-    })
-  );
+    cors ()
+  )
 
   // In development environment the app logs
   app.use(logger("dev"));
